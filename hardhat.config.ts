6b48db897e86ef6c59054eb86c5e07b69ff3016d
David Eiber <davideiber@gmail.com>
2023-04-25 19:01:27 -0400

    Update solc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.19",
+  // networks: {
+  //   hardhat: {
+  //     forking: {
+  //       url: `https://mainnet.infura.io/v3/${process.env.ALCHEMY_KEY}`,
+  //     },
+  //   },
+  // },
 };
 
 export default config;
