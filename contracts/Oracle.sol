4f9fa7f3e406c04b94852800718ffc0ddf84c96a
David Eiber <davideiber@gmail.com>
2023-04-27 12:39:46 -0400

    Integrate duration into pricing, better documentation

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -6,7 +6,7 @@ import "abdk-libraries-solidity/ABDKMath64x64.sol";
 /**
  * @title Oracle
  * @dev Oracle contract returns a price set by the oracle contract owner.
- * The owner can change the base price. The price is inversely proportional to the natural logarithm of the username length.
+ * The owner can change the base price. The price is inversely proportional to the natural logarithm of the username length and is multiplied by the duration.
  */
 contract Oracle is Ownable {
     uint public basePrice;
@@ -17,21 +17,25 @@ contract Oracle is Ownable {
         basePrice = _basePrice;
     }
 
-    error UsernameTooShort();
+    error UsernameTooShortError();
 
     /**
-     * @notice Calculates and returns the price for a username based on its length. The price is determined by the natural logarithm of the username length and is inversely proportional to the length.
-     * @dev The function reverts if the username length is less than 3.The base price is returned if the username length is exactly 3.
+     * @notice Calculates and returns the price for a username based on its length and duration. The price is determined by the natural logarithm of the username length and is inversely proportional to the length.
+     * @dev The function reverts if the username length is less than 3. The base price is returned if the username length is exactly 3.
      * @param usernameLength The length of the username.
+     * @param durationInYears The duration in years for which the price is calculated.
      * @return uint - The current price.
      */
-    function price(uint usernameLength) external view returns (uint) {
+    function price(
+        uint usernameLength,
+        uint8 durationInYears
+    ) external view returns (uint) {
         if (usernameLength < 3) {
-            revert UsernameTooShort();
+            revert UsernameTooShortError();
         }
 
         if (usernameLength == 3) {
-            return basePrice;
+            return basePrice * durationInYears;
         }
 
         // Calculate the natural logarithm of the username length
@@ -45,8 +49,9 @@ contract Oracle is Ownable {
             ABDKMath64x64.fromUInt(usernameLength)
         );
 
-        // Calculate the final price by multiplying the base price, ln(usernameLength), and the factor
+        // Calculate the final price by multiplying the duration in years,  base price, ln(usernameLength), and the factor
         return
+            durationInYears *
             ABDKMath64x64.toUInt(
                 ABDKMath64x64.mul(
                     ABDKMath64x64.fromUInt(basePrice),
@@ -59,7 +64,7 @@ contract Oracle is Ownable {
      * @notice Allows the owner to set a new base price.
      * @param newBasePrice The new base price to be set.
      */
-    function setPrice(uint newBasePrice) external onlyOwner {
+    function setBasePrice(uint newBasePrice) external onlyOwner {
         uint oldBasePrice = basePrice;
         basePrice = newBasePrice;
 
